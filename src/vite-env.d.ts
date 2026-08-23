0040113b0a3548e113d22e2436e71f58de0383d3
Nanda Abi Fahmi <nafal.abi@gmail.com>
2026-08-23 05:25:12 +0000

    feat: add worker api service and frontend env plumbing

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,11 @@
 /// <reference types="vite/client" />
 /// <reference types="@emotion/react/types/css-prop" />
 
+interface ImportMetaEnv {
+  readonly VITE_WORKER_API_URL?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
